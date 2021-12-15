295938070eb1fc7593a174689cb82ca9c5d2b368
Mostafa-N-E <84570342+Mostafa-N-E@users.noreply.github.com>
2021-12-15 10:40:19 +0330

    models

--- core/models.py
+++ core/models.py
@@ -5,6 +5,51 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
+# class Inquiry(models.Model):
+#
+#     class Meta:
+#         abstract = True
+#         ordering = ['id']
+#         verbose_name = 'Inquiry'
+#         verbose_name_plural = 'Inquiries'
+
+
+class RightelMobileInquiry(models.Model):
+    previous_date = models.CharField(max_length=250, verbose_name='previous_date')
+    extra_info = models.IntegerField(verbose_name='extra_info')
+    bill_ID = models.CharField(max_length=250, verbose_name='bill_ID')
+    payment_ID = models.CharField(max_length=250, verbose_name='payment_ID')
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'RightelMobileInquiry'
+        verbose_name_plural = 'RightelMobileInquiry'
+
+
+class PhoneInquiry(models.Model):
+    cycle = models.CharField(max_length=250, verbose_name='cycle')
+    mount = models.IntegerField(verbose_name='mount')
+    bill_ID = models.CharField(max_length=250, verbose_name='bill_ID')
+    payment_ID = models.CharField(max_length=250, verbose_name='payment_ID')
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'PhoneInquiry'
+        verbose_name_plural = 'PhoneInquiries'
+
+
+class DrivingOffensesInquiry(models.Model):
+    plate_number = models.CharField(max_length=250, verbose_name='plate_number')
+    total_amount = models.IntegerField(verbose_name='total_amount')
+    details = models.CharField(max_length=250, verbose_name='plate_number',null=True, blank=True)
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'DrivingOffensesInquiry'
+        verbose_name_plural = 'DrivingOffensesInquiries'
+
+
 class Device(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE,related_name='staff_college')
     discount = models.IntegerField(validators=[MinValueValidator(11), MaxValueValidator(11)],verbose_name='discount')
@@ -17,26 +62,27 @@ class Device(models.Model):
         verbose_name_plural = 'Devices'
 
 
-ZONE_LETTERS = [
-    ('b','ب'),
-    ('g','ج'),
-    #...
-]
-class CarCode(models.Model):
-    zone_letter = models.CharField(max_length=50, choices=ZONE_LETTERS,verbose_name='code')
-    zone_code = models.IntegerField(validators=[MinValueValidator(2), MaxValueValidator(2)],verbose_name='discount')
-    main_code = models.IntegerField(validators=[MinValueValidator(5), MaxValueValidator(5)], verbose_name='main_code')
-
-    class Meta:
-        ordering = ['id']
-        verbose_name = 'CarCode'
-        verbose_name_plural = 'CarCodes'
-
-    def __str__(self):
-        return str(self.main_code)[:1] + str(self.zone_letter) + str(self.main_code)[2:] + str(self.zone_code)
+# ZONE_LETTERS = [
+#     ('b','ب'),
+#     ('g','ج'),
+#     #...
+# ]
+# class CarCode(models.Model):
+#     zone_letter = models.CharField(max_length=50, choices=ZONE_LETTERS,verbose_name='zone_letter')
+#     zone_code = models.IntegerField(validators=[MinValueValidator(2), MaxValueValidator(2)],verbose_name='discount')
+#     main_code = models.IntegerField(validators=[MinValueValidator(5), MaxValueValidator(5)], verbose_name='main_code')
+#
+#     class Meta:
+#         ordering = ['id']
+#         verbose_name = 'CarCode'
+#         verbose_name_plural = 'CarCodes'
+#
+#     def __str__(self):
+#         return str(self.main_code)[:1] + str(self.zone_letter) + str(self.main_code)[2:] + str(self.zone_code)
 
 class Car(Device):
-    code = models.ForeignKey(CarCode, on_delete=models.CASCADE,related_name='staff_college')
+    # code = models.ForeignKey(CarCode, on_delete=models.CASCADE,related_name='staff_college')
+    bar_code = models.CharField(max_length=50, verbose_name='bar_code')
 
     class Meta:
         ordering = ['id']
@@ -48,7 +94,6 @@ class Car(Device):
 
 
 class BasePhone(Device):
-    phone_number = models.IntegerField(validators=[MinValueValidator(11), MaxValueValidator(11)],verbose_name='phone_number')
     is_active = models.BooleanField(verbose_name='active')
 
     class Meta:
@@ -65,6 +110,8 @@ class BasePhone(Device):
 
 
 class Phone(BasePhone):
+    fixed_line_number = models.IntegerField(validators=[MinValueValidator(11), MaxValueValidator(11)], verbose_name='fixed_line_number')
+    inquirys = models.ForeignKey(PhoneInquiry, on_delete=models.CASCADE, related_name='mobile_inquirys')
 
     class Meta:
         ordering = ['id']
@@ -75,8 +122,15 @@ class Phone(BasePhone):
         return
 
 
-
+OPERATORS = [
+    ('raghtel','raghtel'),
+    ('irancel','irancel'),
+    #...
+]
 class Mobile(BasePhone):
+    phone_number = models.IntegerField(validators=[MinValueValidator(11), MaxValueValidator(11)], verbose_name='phone_number')
+    # inquirys = models.ForeignKey(Inquiry, on_delete=models.CASCADE,related_name='mobile_inquirys')
+    operator = models.CharField(max_length=50, choices=OPERATORS,verbose_name='zone_letter')
 
     class Meta:
         ordering = ['id']
