192901c574ee9b22780dd5720b27e23276d7c68c
Mostafa-N-E <84570342+Mostafa-N-E@users.noreply.github.com>
2021-12-19 22:39:50 +0330

    set urls

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 
-
 from . import views
 
 app_name = 'core'
 
 urlpatterns = [
 
-    path('irancel-mobile-bill-inquiry/', views.BillInquiryApi.as_view(), name='bill_inquiry'),
-
+    path('bill-inquiry/', views.BillInquiryApi.as_view(), name='bill_inquiry'),
+    path('delete-bill-inquiry/', views.DeleteInquiry.as_view(), name='delete_bill_inquiry'),
+    path('update-bill-inquiry/', views.UpdateInquiry.as_view(), name='update_bill_inquiry'),
+    path('retrieve-bill-inquiry/', views.RetrieveInquiry.as_view(), name='retrieve_bill_inquiry'),
 
 ]
