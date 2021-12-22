fd8e023989646f57f8b259927d46734e8659b933
ubuntu <m.nazari.est@gmail.com>
2021-12-21 23:12:04 -0800

    final commit

--- core/serializers.py
+++ core/serializers.py
@@ -5,8 +5,8 @@ class BillInquirySerializer(serializers.ModelSerializer):
     device_id = serializers.IntegerField()
     class Meta:
         model = Inquiry
-        # fields = '__all__'
-        fields = ['device','Description','Code','Amount','PreviousDate', 'CurrentDate', 'PaymentDate', 'FullName', 'BillID', 'device_id']
+        fields = '__all__'
+        # fields = ['device','Description','Code','Amount','PreviousDate', 'CurrentDate', 'PaymentDate', 'FullName', 'BillID', 'device_id']
 
 
     #
@@ -29,8 +29,8 @@ class DeviceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Device
-        # fields = '__all__'
-        fields = ['Number', 'Operator','TypeLine','owner', 'name', 'last_inquiry', 'device_type', 'description']
+        fields = '__all__'
+        # fields = ['Number', 'Operator','TypeLine','owner', 'name', 'last_inquiry', 'device_type', 'description']
 
 # ---------------------------------------------------------------------------------------------------------
 
