f5d58963a583d283348e46ae4b4dce6f1385e516
ruhollahmozafari <ruhollahmozafari@gmail.com>
2022-05-18 10:01:01 +0430

    commit before pass it to dear Sara

--- property/permissions.py
+++ property/permissions.py
@@ -3,4 +3,5 @@ from rest_framework import permissions
 
 class CustomePermissions(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.role == 1
+
+        return request.user.role == 1   
